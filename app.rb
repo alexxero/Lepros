1fc160b99ca2dfc50430cd53643a464583f1c853
alexxero <alexxero@mail.ru>
2016-04-20 13:26:54 +0300

    init blogs

---
+++ app.rb
@@ -0,0 +1,12 @@
+require 'rubygems'
+require 'sinatra'
+require 'sinatra/reloader'
+
+get '/' do
+	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+end
+
+get '/NewPost' do
+  erb :NewPost
+end
+
